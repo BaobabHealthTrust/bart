d316fea65c4a6e8f48c3c8835430f749fc11392b
Deliwe <dbnkhoma@gmail.com>
2014-02-21 15:27:55 +0200

    added queries pulling all male patients with female observations

--- app/models/validation_rule.rb
+++ app/models/validation_rule.rb
@@ -65,4 +65,70 @@ class ValidationRule < ActiveRecord::Base
 				WHERE e.encounter_type IS NULL AND p.registration_date <= DATE('#{date}');
 			").map(&:patient_id)
 	end
+
+  def self.male_patients_with_pregnant_obs(end_date = Date.today)
+    @end_date = end_date.to_date.strftime('%Y-%m-%d 23:59:59')
+
+    pregnant_ids = [Concept.find_by_name("PREGNANT").concept_id,
+                    Concept.find_by_name("PREGNANT WHEN ART WAS STARTED").concept_id]
+
+    male_pats_with_preg_obs = ValidationRule.find_by_sql("
+                                      SELECT prd.patient_id, p.gender,
+                                             prd.registration_date, o.concept_id,
+                                             o.value_coded, o.obs_datetime
+                                      FROM patient_registration_dates prd
+	                                      INNER JOIN patient p ON p.patient_id = prd.patient_id
+	                                        AND p.voided = 0
+                                        INNER JOIN obs o ON o.patient_id = p.patient_id
+                                         AND o.voided = 0
+                                      WHERE p.gender = 'Male'
+                                      AND (o.concept_id IN (#{pregnant_ids.join(',')})
+                                        OR o.value_coded IN (#{pregnant_ids.join(',')}))
+                                      AND o.obs_datetime <= '#{@end_date}'
+                                      GROUP BY prd.patient_id").collect{|p| p.patient_id}
+  end
+
+  def self.male_patients_with_breastfeeding_obs(end_date = Date.today)
+    @end_date = end_date.to_date.strftime('%Y-%m-%d 23:59:59')
+
+    breastfeeding_id = Concept.find_by_name("BREASTFEEDING").concept_id
+
+    male_pats_with_breastfeed_obs = ValidationRule.find_by_sql("
+                                      SELECT prd.patient_id, p.gender,
+                                             prd.registration_date, o.concept_id,
+                                             o.value_coded, o.obs_datetime
+                                      FROM patient_registration_dates prd
+	                                      INNER JOIN patient p ON p.patient_id = prd.patient_id
+	                                        AND p.voided = 0
+                                        INNER JOIN obs o ON o.patient_id = p.patient_id
+                                         AND o.voided = 0
+                                      WHERE p.gender = 'Male'
+                                      AND (o.concept_id = #{breastfeeding_id}
+                                        OR o.value_coded = #{breastfeeding_id})
+                                      AND o.obs_datetime <= '#{@end_date}'
+                                      GROUP BY prd.patient_id").collect{|p| p.patient_id}
+  end
+
+  def self.male_patients_with_family_planning_methods_obs(end_date = Date.today)
+    @end_date = end_date.to_date.strftime('%Y-%m-%d 23:59:59')
+
+    family_planning_ids = [Concept.find_by_name("FAMILY PLANNING METHOD").concept_id,
+                         Concept.find_by_name("CURRENTLY USING FAMILY PLANNING METHOD").concept_id]
+
+    male_pats_with_family_planning_obs = ValidationRule.find_by_sql("
+                                            SELECT prd.patient_id, p.gender,
+                                                   prd.registration_date, o.concept_id,
+                                                   o.value_coded, o.obs_datetime
+                                            FROM patient_registration_dates prd
+	                                            INNER JOIN patient p ON p.patient_id = prd.patient_id
+	                                              AND p.voided = 0
+                                              INNER JOIN obs o ON o.patient_id = p.patient_id
+                                               AND o.voided = 0
+                                            WHERE p.gender = 'Male'
+                                            AND (o.concept_id IN (#{family_planning_ids.join(',')})
+                                              OR o.value_coded IN (#{family_planning_ids.join(',')}))
+                                            AND o.obs_datetime <= '#{@end_date}'
+                                            GROUP BY prd.patient_id").collect{|p| p.patient_id}
+  end
+
 end
