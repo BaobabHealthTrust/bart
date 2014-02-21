5989e854bd37382d8d8c25bb56b43049f0117374
timmtonga <timmtonga@gmail.com>
2014-02-21 13:24:44 +0200

    added function to validate presence of start reasons

--- app/models/validation_rule.rb
+++ app/models/validation_rule.rb
@@ -11,6 +11,10 @@ class ValidationRule < ActiveRecord::Base
   def self.validate_presence_of_start_reason
     #This function checks for patients who do not have a reason for starting ART
 
+    start_reason_id = PersonAttributeType.find_by_name("Reason antiretrovirals started").id
+    art_patients = Patient.find_by_sql("SELECT patient_id FROM patient_registration_dates where patient_id not in
+                      (SELECT person_id from person_attribute where person_attribute_type_id = #{start_reason_id} and voided = 0)")
 
+    return art_patients
   end
 end
