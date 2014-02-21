98d0a7bedca13d9fccf45da9c263eb7a24f6199b
Blessings <blessings.mhango@gmail.com>
2014-02-21 14:01:30 +0200

    added Death date > last encounter > DOB

--- app/models/validation_rule.rb
+++ app/models/validation_rule.rb
@@ -13,4 +13,20 @@ class ValidationRule < ActiveRecord::Base
 
 
   end
+
+  def self.death_date_less_than_last_encounter_date_and_less_than_date_of_birth(end_date = Date.today)
+    PatientProgram.find_by_sql("SELECT DISTINCT(prd.patient_id)
+																FROM patient_registration_dates prd
+																INNER JOIN patient p 
+																ON p.patient_id = prd.patient_id
+																WHERE p.birthdate IS NOT NULL 
+																AND p.death_date IS NOT NULL 
+																AND p.death_date < (SELECT MAX(encounter_datetime)
+                       															 FROM encounter e 
+                       															 WHERE e.patient_id = prd.patient_id) 
+                                AND (SELECT MAX(encounter_datetime)
+                       							 FROM encounter e 
+                       							 WHERE e.patient_id = prd.patient_id) < p.birthdate;").length
+    
+  end 
 end
