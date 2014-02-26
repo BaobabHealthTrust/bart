84a6d2e599e58ca0dc952bdea975d0eb0037397d
chawanangwa lupafya <chawanangwa.lupafya@baobabhealth.org>
2014-02-26 14:44:35 +0200

    Task 51 & 59: sum of reason starting ART equal to total registered & Sum of tb = total alive and on ART

--- app/models/cohort_validation.rb
+++ app/models/cohort_validation.rb
@@ -92,6 +92,47 @@ class CohortValidation
 							self.cum_cohort['patients_with_unknown_outcomes']]			 					
 		return self.feed_values(validation_rule.expr, values)		
 	end
-		  
+
+	def validate_sum_of_tb_equal_total_alive_and_on_ART
+		#Task 59
+		#Sum of tb = total alive and on ART
+
+		validation_rule = ValidationRule.find_by_desc("TB not suspected, TB suspected, TB confirmed not yet/currently not on TB treatment, TB confirmed on TB treatment, and Unknown TB status should add up to Total alive and on ART")
+
+		return nil if validation_rule.blank?
+
+		values = [self.cohort_object['alive_on_ART_patients'],
+				 			self.cohort_object['tb_not_suspected_patients'],
+				 			self.cohort_object['tb_suspected_patients'],
+				 			self.cohort_object['tb_confirmed_not_on_treatment_patients'],
+				 			self.cohort_object['tb_confirmed_on_treatment_patients'],
+				 			self.cohort_object['tb_status_unknown_patients']
+				 			]
+		return self.feed_values(validation_rule.expr, values)
+	end
+	
+	def validate_sum_of_reason_starting_ART_equal_total_registered
+		#Task 51
+		#sum of reason starting ART equal to total registered
+
+		validation_rule = ValidationRule.find_by_desc("[CUMULATIVE] Presumed severe HIV disease in infants, Confirmed HIV infection in infants (PCR), WHO stage 1 or 2, CD4 below threshold, , Children 12-23 mths, Breastfeeding mothers, Pregnant women, WHO stage 3, WHO stage 4, and Unknown/other reason outside ")
+		return nil if validation_rule.blank?
+
+		values = [self.cohort_object['all_patients'],
+							self.cohort_object['infants_presumed_severe_HIV'],
+							self.cohort_object['infants_PCR'],
+							self.cohort_object['who_stage_1_or_2_cd4'],
+							self.cohort_object['who_stage_2_lymphocyte'],
+							self.cohort_object['child_patients'],
+							self.cohort_object['breastfeeding_mothers'],
+							self.cohort_object['started_cause_pregnant'],
+							self.cohort_object['who_stage_3'],
+							self.cohort_object['who_stage_4'],
+							self.cohort_object['start_reason_other']
+				 		 ]
+		return self.feed_values(validation_rule.expr, values)
+	end
+
+			  
 end
 
